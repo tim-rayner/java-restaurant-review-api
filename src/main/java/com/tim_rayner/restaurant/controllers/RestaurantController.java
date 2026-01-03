ccde349fb69e3c538c7eaac12376b658c6a849b5
tim-rayner <tim.rayner2020@gmail.com>
2026-01-03 00:42:17 +0000

    chore(entity): added basic restaurant entity

--- src/main/java/com/tim_rayner/restaurant/controllers/RestaurantController.java
+++ src/main/java/com/tim_rayner/restaurant/controllers/RestaurantController.java
@@ -1,13 +1,15 @@
 package com.tim_rayner.restaurant.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/restaurant")
 public class RestaurantController {
 
     @GetMapping("/helloworld")
     public String helloWorld() {
-        return "Hello Worlds!";
+        return "Hello World!";
     }
 }
---
+++ src/main/java/com/tim_rayner/restaurant/entities/Restaurant.java
@@ -0,0 +1,45 @@
+package com.tim_rayner.restaurant.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="RESTAURANTS")
+public class Restaurant {
+    @Id
+    @GeneratedValue
+    @Getter @Setter
+    private Long id;
+
+    @Column(name="NAME")
+    @Getter @Setter
+    private String name;
+
+    @Column(name="BIO")
+    @Getter @Setter
+    private String bio;
+
+    @Column(name="OVERALL_RATING")
+    @Getter @Setter
+    private double overallRating;
+
+    @Column(name="PEANUT_RATING")
+    @Getter @Setter
+    private double peanutRating;
+
+    @Column(name="EGG_RATING")
+    @Getter @Setter
+    private double eggRating;
+
+    @Column(name="DAIRY_RATING")
+    @Getter @Setter
+    private double dairyRating;
+
+
+
+}
