673a951b4d1b289d6a5c90721d0510b80a41ee9b
tim-rayner <tim.rayner2020@gmail.com>
2026-01-03 09:06:26 +0000

    chore(pojo): added basic admin user action

---
+++ src/main/java/com/tim_rayner/restaurant/actions/AdminReviewAction.java
@@ -0,0 +1,9 @@
+package com.tim_rayner.restaurant.actions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class AdminReviewAction {
+    @Getter @Setter
+    private Boolean acceptReview;
+}
